17dfaf7da598025fc5ad3a3a61d551a2bd7da2d3
James Trinklein <theotherjim@gmail.com>
2014-01-12 17:09:17 -0800

    adding configurable drop, restructuring code a little, better messaging

--- src/io/github/jamest/breakfast/BreakFast.java
+++ src/io/github/jamest/breakfast/BreakFast.java
@@ -3,7 +3,6 @@ package io.github.jamest.breakfast;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,33 +11,67 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Collection;
-import java.util.HashMap;
+import java.io.Serializable;
+import java.util.List;
 
 public class BreakFast extends JavaPlugin
         implements Listener
 {
-    private HashMap<String,Boolean> bfEnabled;
-    private HashMap<String,Boolean> dropEnabled;
+    private final String permissionString = "breakfast.use";
 
-    private final String nameTag = ChatColor.GREEN+"["+ChatColor.BLUE+"Break"+ChatColor.RED+"Fast"+ChatColor.GREEN+"]"+ChatColor.WHITE;
+    private class PlayerSettings implements Serializable {
+        public PlayerSettings(Boolean enabled, Boolean drop){
+            this.setEnabled(enabled);
+            this.setDrop(drop);
+        }
+
+        private Boolean drop;
+        public Boolean getDrop() {
+            return this.drop;
+        }
+        public void setDrop(Boolean enabled) {
+            this.drop = enabled;
+        }
+        public void toggleDrop() {
+            setDrop(!getDrop());
+        }
+
+        private Boolean plugin;
+        public Boolean getEnabled() {
+            return this.plugin;
+        }
+        public void setEnabled(Boolean enabled) {
+            this.plugin = enabled;
+        }
+        public void toggleEnabled() {
+            setEnabled(!getEnabled());
+        }
+    }
+
+    private Boolean dropEnabledDefault;
+    private String name;
+
+    private final String PluginMetadataKey = "jamest.breakfast.enable";
+    private final String DropMetadataKey = "jamest.breakfast.drop";
+
+
+    private final String NameTag = ChatColor.GREEN+"["+ChatColor.BLUE+"Break"+ChatColor.RED+"Fast"+ChatColor.GREEN+"]"+ChatColor.WHITE;
 
     public void onEnable()
     {
-        if(this.dropEnabled == null) {
-            this.dropEnabled = new HashMap<String, Boolean>();
-        }
+        saveDefaultConfig();
 
-        if(this.bfEnabled == null) {
-            this.bfEnabled = new HashMap<String, Boolean>();
-        }
-
+        this.dropEnabledDefault = (Boolean)getConfig().get("defaults.enable-drop");
         getServer().getPluginManager().registerEvents(this, this);
         PluginDescriptionFile pdf = getDescription();
+        this.name = pdf.getName();
+
         getLogger().info("version " + pdf.getVersion() + " has been enabled.");
         saveDefaultConfig();
     }
@@ -48,40 +81,99 @@ public class BreakFast extends JavaPlugin
         getLogger().info("version " + pdf.getVersion() + " has been disabled.");
     }
 
-    private boolean getDropEnabled(String playerName) {
-        return this.dropEnabled != null
-                && this.dropEnabled.containsKey(playerName)
-                && this.dropEnabled.get(playerName);
+    private PlayerSettings getMetadataForPlayer(Player player) {
+        List<MetadataValue> enableMetadata = player.getMetadata(this.PluginMetadataKey);
+
+        Boolean enabled = false;
+        for(MetadataValue value: enableMetadata) {
+            if(value.getOwningPlugin().getDescription().getName().equals(this.name)) {
+                enabled = value.asBoolean();
+            }
+        }
+
+        List<MetadataValue> dropMetadata = player.getMetadata(this.DropMetadataKey);
+        Boolean drop = this.dropEnabledDefault;
+        for(MetadataValue value: dropMetadata) {
+            if(value.getOwningPlugin().getDescription().getName().equals(this.name)) {
+                drop = value.asBoolean();
+            }
+        }
+
+        PlayerSettings playerSettings = new PlayerSettings(enabled, drop);
+        return playerSettings;
+    }
+
+    private void setMetadataForPlayer(Player player, PlayerSettings playerSettings) {
+        player.setMetadata(this.PluginMetadataKey, new FixedMetadataValue(this, playerSettings.getEnabled()));
+        player.setMetadata(this.DropMetadataKey, new FixedMetadataValue(this, playerSettings.getDrop()));
     }
 
     private String getEnabledString(Boolean enabled) {
         String msg = enabled
                 ? ChatColor.GREEN + "enabled" + ChatColor.WHITE
                 : ChatColor.RED + "disabled" + ChatColor.WHITE;
+        return msg;
+    }
+
+    private Boolean hasPermission(Permissible p) {
+        return p.hasPermission(permissionString);
+    }
+
+    private Boolean checkPermissionWithMessage(CommandSender sender) {
+        if(hasPermission(sender)) {
+            return true;
+        }
+
+        sender.sendMessage("You do not have sufficient permissions to use " + this.NameTag);
+
+        return false;
+    }
+
+    private void handleBreakFastCommand(CommandSender sender) {
+        if(!checkPermissionWithMessage(sender)) {
+            return;
+        }
+
+        Player player = (Player)sender;
+        PlayerSettings settings = getMetadataForPlayer(player);
+        if(!settings.getEnabled()) {
+            settings.setDrop(this.dropEnabledDefault);
+        }
+        settings.toggleEnabled();
+        setMetadataForPlayer(player, settings);
+
+        String msg = " " + getEnabledString(settings.getEnabled());
+        if(settings.getEnabled()) {
+            msg += " with drop " + getEnabledString(settings.getDrop());
+        }
+        sender.sendMessage(this.NameTag + msg);
+    }
+
+    private void handleBreakFastDropCommand(CommandSender sender) {
+        if(!checkPermissionWithMessage(sender)) {
+            return;
+        }
+
+        Player player = (Player)sender;
+        PlayerSettings settings = getMetadataForPlayer(player);
+        settings.toggleDrop();
+        setMetadataForPlayer(player, settings);
+
+        String msg = getEnabledString(settings.getDrop());
+        sender.sendMessage(this.NameTag + " drop " + msg);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        Player player = (Player)sender;
+        // handle /BreakFast and /bf
         if(cmd.getName().equalsIgnoreCase("breakfast") || cmd.getName().equalsIgnoreCase("bf")) {
-            if(player.hasPermission("breakfast.use")) {
-                Boolean enable = !this.bfEnabled.containsKey(player.getName()) || !this.bfEnabled.get(player.getName());
-                this.bfEnabled.put(player.getName(), enable);
-                String msgEnable = getEnabledString(enable);
-                String msgDrop = getEnabledString(getDropEnabled(player.getName()))
-                sender.sendMessage(this.nameTag + msgEnable + " with drop " + msgDrop);
-
-            }
-            else {
-                sender.sendMessage("You do not have sufficient permissions to use " + nameTag);
-            }
+            handleBreakFastCommand(sender);
             return true;
         }
+
+        // handle /BreakFastDrop and /bfd
         if (cmd.getName().equalsIgnoreCase("breakfastdrop") || cmd.getName().equalsIgnoreCase("bfd")) {
-            Boolean drop = !getDropEnabled(player.getName());
-            this.dropEnabled.put(player.getName(),drop);
-            String msg = getEnabledString(drop);
-            sender.sendMessage(this.nameTag + " drop " + msg);
+            handleBreakFastDropCommand(sender);
             return true;
         }
 
@@ -91,19 +183,27 @@ public class BreakFast extends JavaPlugin
     @EventHandler
     public void playerInteract(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        if(!this.bfEnabled.containsKey(player.getName()) || !this.bfEnabled.get(player.getName())) {
+        PlayerSettings settings = getMetadataForPlayer(player);
+
+        if(!settings.getEnabled()) {
             return;
         }
 
-        World world = player.getWorld();
         Block block = e.getClickedBlock();
-        Boolean drop = getDropEnabled(player.getName());
 
         if ((e.getAction() == Action.LEFT_CLICK_BLOCK)
                 && block.getLocation().getY() > 0.0D
-                && block.getType() != Material.BEDROCK
-                && player.hasPermission("breakfast.use")) {
-            if(drop) {
+                && block.getType() != Material.BEDROCK) {
+
+            // if player has plugin enabled but no longer has permission,
+            //  disable plugin
+            if(!hasPermission(player)) {
+                settings.setEnabled(false);
+                setMetadataForPlayer(player, settings);
+            }
+
+            // check for drop
+            if(settings.getDrop()) {
                 block.breakNaturally();
             } else {
                 block.setType(Material.AIR);
